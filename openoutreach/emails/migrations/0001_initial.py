8f98556f8b1ddc0a4dfe8e901bbc280e2c43ec02
guerra2fernando <fern2gue@gmail.com>
2026-07-07 21:10:29 +0400

    add missing emails app migrations for Mailbox model

---
+++ openoutreach/emails/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# Generated manually
+
+from django.db import migrations, models
+import openoutreach.emails.models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='Mailbox',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('host', models.CharField(default='smtp.gmail.com', max_length=255)),
+                ('port', models.PositiveIntegerField(default=587)),
+                ('username', models.CharField(max_length=320, unique=True)),
+                ('password', models.CharField(max_length=255)),
+                ('from_address', models.EmailField(max_length=320)),
+                ('daily_limit', models.PositiveIntegerField(default=50)),
+            ],
+            options={
+                'verbose_name_plural': 'Mailboxes',
+            },
+            managers=[
+                ('objects', openoutreach.emails.models.MailboxManager()),
+            ],
+        ),
+    ]
