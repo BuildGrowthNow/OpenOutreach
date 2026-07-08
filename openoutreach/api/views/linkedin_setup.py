5c2b985034027a8e810f4002a6fa08e4a9a4fed7
guerra2fernando <fern2gue@gmail.com>
2026-07-08 08:13:30 +0400

    fix build

--- openoutreach/api/views/linkedin_setup.py
+++ openoutreach/api/views/linkedin_setup.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
 class LinkedInCookieInstructionsView(APIView):
     """
     API view for LinkedIn cookie setup instructions.
